07a9bf527da1fe806016060f4ac575cfcd1061fa
sdl1wrkhremb <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-08-05 07:12:05 -0400

    Delete beginner-projects/madlibs directory

--- beginner-projects/madlibs/random_madlibs.py
+++
@@ -1,9 +0,0 @@
-"""this program will select from a random madlib from a folder
-and present to the user. """
-
-import random
-
-from sample import code, haunted_house, zombie
-
-x = random.choice([code, haunted_house, zombie])
-x.madlib()
