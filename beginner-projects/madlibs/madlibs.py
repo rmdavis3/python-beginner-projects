796173444e6a7f41a6b100283d36885e05ae910c
sdl1wrkhremb <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-08-05 07:09:17 -0400

    Add files via upload
    uploading code

---
+++ beginner-projects/madlibs/madlibs.py
@@ -0,0 +1,12 @@
+""" # madlib beginner project
+# https://www.youtube.com/watch?v=8ext9G7xspg&t=100s """
+
+adjective1 = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adjective1} ! It makes me so excited all the time \
+because I love to {verb1}. Stay hydated and {verb2} like you are {famous_person}!"
+
+print(madlib)
