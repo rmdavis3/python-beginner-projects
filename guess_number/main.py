b87c7b354277edc28bf7493822713f9e0abe642a
Ryan Davis <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-08-14 17:08:01 -0400

    updated game so user can choose game type

--- guess_number/main.py
+++ guess_number/main.py
@@ -2,8 +2,13 @@ import guess_number
 
 ryans_game = guess_number
 
-# ryans_game.user_guess()
-ryans_game.computer_guess()
-
-# ryans_game.user_guess2(10)
-# ryans_game.computer_guess2(10)
+game_type = int(input(
+    "Enter 1 if you want to guess, enter 2 if you want the computer to guess: "))
+if game_type == 1:
+    # ryans_game.user_guess()
+    ryans_game.user_guess2(10)
+elif game_type == 2:
+    # ryans_game.computer_guess()
+    ryans_game.computer_guess2(10)
+else:
+    print("Invalid entry")
